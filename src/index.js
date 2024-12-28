dcce97cc48129b48324d948957e07a2af3ba8808
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2024-12-28 23:10:32 +0530

    Feat: added express rate limiter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 2*60*100,     // Every 2 minutes
+    max: 3                  // Limit each IP to 3 request per 'window'
+})
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
